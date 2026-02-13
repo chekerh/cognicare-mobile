fd17d66062a29673cbf105f73c30234a04ba44de
sarra1201 <sarra.chmek@esprit.tn>
2026-02-13 01:35:21 +0100

    minSdk 23 for record_android, add Gradle cache in CI

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.cognicare.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23  // Required by record_android (flutter.minSdkVersion is 21)
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
